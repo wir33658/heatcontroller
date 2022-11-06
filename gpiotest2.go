1b9fdf743437f2e37e356900dbcb4209a5189f9c
Robert Weissmann <2758936+wir33658@users.noreply.github.com>
2022-11-06 11:33:13 +0100

    minor

--- gpiotest2.go
+++ gpiotest2.go
@@ -5,25 +5,31 @@ import (
     "time"
 )
 
-var arr1 = [4]int{1,1,0,0}
-var arr2 = [4]int{0,1,0,0}
+var arr3 = [4]int{1,1,0,0}
+var arr4 = [4]int{0,1,0,0}
 
 func main() {
     fmt.Println("GPIO Test2")
     for true {
-        move()
+        move2()
     }
 }
 
-func move99() {
-    var arrOUT = append(arr1[1:], arr1[:1]...)               // [1:]+arr1[:1] # rotates array values of 1 digit
-    arr1 = arr2
-    copy(arr2[0:], arrOUT)
+func move2() {
+    var arrOUT = append(arr3[1:], arr3[:1]...)               // [1:]+arr1[:1] # rotates array values of 1 digit
+    arr3 = arr4
+    copy(arr4[0:], arrOUT)
     //GPIO.output(chan_list, arrOUT)
-    set(arrOUT)
+    set2(arrOUT)
     time.Sleep(time.Second * 1)
 }
 
-func set(arr []int) {
+func set2(arr []int) {
     fmt.Println("arr:", arr)
+
+    if(arr[0] == 0){
+        fmt.Println("17 : Low")
+    } else {
+        fmt.Println("17 : High")
+    }
 }
